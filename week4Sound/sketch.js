7d52188955a65bd3daac9504c0c2cf537f19619e
JoeRobbi <joegawlik@yahoo.com>
2019-09-11 23:58:33 -0500

    sounds

---
+++ week4Sound/sketch.js
@@ -0,0 +1,17 @@
+var song1;
+
+function preLoad(){
+  song1 = loadSound("assets/BOOM.mp3");
+}
+
+function setup() {
+  // put setup code here
+  createCanvas(800, 800);
+  background(255,0,0);
+  song1.play();
+
+}
+
+function draw() {
+  // put drawing code here
+}
