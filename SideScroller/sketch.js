957087e027dca27d52c3b70d203b48a1d00718f3
jrg007guy <32406469+jrg007guy@users.noreply.github.com>
2019-11-16 16:28:55 -0600

    Health and snowman damge

--- SideScroller/sketch.js
+++ SideScroller/sketch.js
@@ -17,39 +17,43 @@ var backgroundPic;
 var losePic;
 var winPic;
 var resetTimer = 100;
-health = 3;
+var health = 3;
+var levelArt;
+var snowMan;
+var damageState = 0;
+var damageTimer = 0;
 //virtual camera
 //move the mouse around
 //because the camera is following it
 var frame;
 //the scene is twice the size of the canvas
-var SCENE_W = 3000;
+var SCENE_W = 1600;
 var SCENE_H = 800;
 
 function setup() {
   createCanvas(800, 800);
 
   splashPic = loadImage("assets/SplashScreen.png");
-  backgroundPic = loadImage("assets/SnowbackgroundOriginal.png");
+  backgroundPic = loadImage("assets/Snowbackground.png");
   losePic = loadImage("assets/You_lose_screen.png");
   winPic = loadImage("assets/You_win.png");
 
 
 
   player = createSprite(200, 200);
-  var playerAnimControl = player.addAnimation('Idle');
-  player.addAnimation('Running', 'assets/Player/Player1.png', 'assets/Player/Player2.png', 'assets/Player/Player3.png', 'assets/Player/Player4.png', 'assets/Player/Player5.png', 'assets/Player/Player6.png');
+  var playerAnimControl = player.addAnimation('Running', 'assets/Player/Player1.png', 'assets/Player/Player2.png',
+  'assets/Player/Player3.png', 'assets/Player/Player4.png', 'assets/Player/Player5.png', 'assets/Player/Player6.png');
+
   player.addAnimation('Jumped', 'assets/Player/Player3.png');
-  player.addAnimation('PowerUpRunning', 'assets/Player/Player3.png');
-  player.addAnimation('PowerUpIdle', 'assets/Player/Player3.png');
+  player.setCollider('rectangle', 0, 0, 500, 300);
   player.debug = true;
 
   //scale and offset sprite
   player.scale = 0.2;
-  playerAnimControl.offX = 5;
-  playerAnimControl.offY = 5;
+  playerAnimControl.offX = -100;
+  playerAnimControl.offY = -50;
 
-  pickUp = createSprite(350, 215);
+  pickUp = createSprite(700, 370);
   var pickUpAnim = pickUp.addAnimation('item', 'assets/PickUp/pickUp0.gif', 'assets/PickUp/pickUp1.gif', 'assets/PickUp/pickUp2.gif',
     'assets/PickUp/pickUp3.gif', 'assets/PickUp/pickUp4.gif', 'assets/PickUp/pickUp5.gif', 'assets/PickUp/pickUp6.gif', 'assets/PickUp/pickUp7.gif',
     'assets/PickUp/pickUp8.gif', 'assets/PickUp/pickUp9.gif', 'assets/PickUp/pickUp10.gif');
@@ -60,16 +64,18 @@ function setup() {
 
   pickUp.scale = 0.3;
 
-  snowMan = createSprite(1050, 315);
-  var snowManAnim = snowMan.addAnimation('idle');
+  snowMan = createSprite(500, 315);
+  var snowManAnim = snowMan.addAnimation('idle', 'assets/Evil_snowman.png');
 
-  snowMan.setCollider('circle', 0, 0, 100);
+  snowMan.setCollider('rectangle', 0, 0, 400, 700);
   snowMan.debug = true;
 
-  snowMan.scale = 0.5;
+  snowMan.scale = 0.2;
+  snowManAnim.offX = 0;
+  snowManAnim.offY = 0;
 
   platform = createSprite(1600, 400);
-  platform.addImage(loadImage('assets/level.png'));
+  platform.addImage(loadImage('assets/LevelArt.png'));
 
   player.depth = 10;
 }
@@ -82,10 +88,10 @@ function draw() {
 
       break;
     case 1:
-      image(backgroundPic, -200, -150, 2000 * 1.5, 600 * 1.5);
+      image(backgroundPic, -600, -1400, 2000 * 4, 600 * 6);
       game();
       timer--;
-      if (timer < 1) {
+      if (timer < 1 || health <= 0) {
         timer = 600;
         gameState = 2;
       }
@@ -100,8 +106,9 @@ function draw() {
       camera.on();
       resetTimer = 100;
       fill('white');
-      text("Loading...", 65, 600);
-      player.position.x = 200;
+      textSize(50);
+      text("Loading...", 400, 400);
+      player.position.x = 300;
       player.position.y = 100;
       gameState = 0;
     }
@@ -114,10 +121,11 @@ function draw() {
       background('black');
       camera.on();
       fill('white');
-      text("Loading...", 65, 600);
+      textSize(50);
+      text("Loading...", 400, 400);
       resetTimer--;
       if (resetTimer < 1) {
-        player.position.x = 200;
+        player.position.x = 300;
         player.position.y = 100;
         resetTimer = 100;
         gameState = 1;
@@ -141,10 +149,11 @@ function game() {
   fill('Red');
   textSize(50);
   text('Health:'+ health, camera.position.x-350, camera.position.y-300);
+  text('Timer:'+ timer, camera.position.x+100, camera.position.y-300);
 
   //a camera is created automatically at the beginning
   //.5 zoom is zooming out (50% of the normal size)
-  camera.zoom = 1;
+  camera.zoom = 1.1;
 
   //set the camera position to the player position
 
@@ -177,10 +186,16 @@ function game() {
   if (keyIsDown(RIGHT_ARROW))
     player.velocity.x = 5;
 
-  //instead of checking the colliders or bounding box overlaps
-  //I can just check a point against a collider
-  if (pickUp.overlapPoint(player.position.x, player.position.y))
-    pickUp.changeAnimation('item_taken');
+  // //instead of checking the colliders or bounding box overlaps
+  // //I can just check a point against a collider
+  // if (pickUp.overlapPoint(player.position.x, player.position.y))
+  //   pickUp.changeAnimation('item_taken');
+
+  if(player.overlap(snowMan))
+  {
+    playerDamage();
+    damageState = 1;
+  }
 
   //Or check a point against the pixels of a sprite animation or image
   //if the bottom of the player is not overlapping with the non transparent pixels
@@ -213,5 +228,25 @@ function playerAnimState() {
         playerTimer = 0;
       }
       break;
+  }
+}
+
+function playerDamage(){
+  switch (damageState) {
+    case 0:
+      //idel state
+      break;
+    case 1:
+    snowMan.remove();
+    damageTimer++
+    if (damageTimer >= 20) {
+      snowMan.position.x =-1000;
+      damageTimer = 0;
+      health = health -1;
+      damageState = 0;
+    }
+
+    break;
+
   }
 }
